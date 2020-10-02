87c102a29a04ea3309220b849f2ba559f8935243
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2020-10-02 12:54:30 +0200

    11MyCogs geupdate

--- html/11 Cogs and Gears/script.js
+++ html/11 Cogs and Gears/script.js
@@ -10,35 +10,31 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
-let img,angle,img2,x;
-x = 0;
-
+let img,angle,img2
 img = new Image();
 img.src = "img/cog.png";
 
 img2 = new Image();
-img2.src = "img/landscape.jpg"
-
+img2.src = "img/cog2.png";
 
 angle = 0.1;
 
-img2.addEventListener('load',()=>{
+img.addEventListener('load',()=>{
   setInterval(animate,10)
 })
 
-img2.addEventListener('load',()=>{
-  setInterval(animate,10)
-})
+function animate (){
+    context.clearRect(0,0,height,width);
+    context.save();
+    context.translate(300,300);
+    context.rotate(angle);
+    context.drawImage(img,-150,-150,300,300);
+    context.restore();
 
-function animate(){
-  context.clearRect(0,0,width,height);
-  context.drawImage(img2,0,0,width,height);
-  context.save();
-  context.translate(x,300);
-  context.rotate(angle);
-
-  context.drawImage(img,-150,-150,300,300);
-  context.restore();
-  angle += 0.01;
-  x+=1;
+    context.save();
+    context.translate(340,565);
+    context.rotate(-angle);
+    context.drawImage(img2,-150,-150,300,300);
+    context.restore();
+    angle += 0.01;
 }
