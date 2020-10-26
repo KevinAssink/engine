8d63aa0be0472d9e63849e1cb221abdafb4fbbc6
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2020-10-26 16:10:01 +0100

    Added the Second (blue) tank

--- html/18TankSheet/script.js
+++ html/18TankSheet/script.js
@@ -18,12 +18,20 @@ gTank = {};
 gTank.animationArray = [1,2,3,4,5,6,7,8];
 gTank.index = 0;
 
-
-
 gTank.draw = function() {
     gTank.sx = gTank.animationArray[gTank.index]%8 *84;
     gTank.sy = Math.floor(gTank.animationArray[gTank.index]/8)*84;
     context.drawImage(spriteSheet,gTank.sx,gTank.sy,84,84,100,100,84,84);
+}
+
+bTank = {};
+bTank.animationArray = [9,10,11,12,13,14,15,16];
+bTank.index = 0;
+
+bTank.draw = function() {
+    bTank.sx = bTank.animationArray[bTank.index]%8 *84;
+    bTank.sy = Math.floor(bTank.animationArray[bTank.index]/8)*84;
+    context.drawImage(spriteSheet,bTank.sx,bTank.sy,84,84,200,100,84,84);
 }
 
 spriteSheet.addEventListener('load',()=>{
@@ -41,5 +49,12 @@ function animate(){
   if(gTank.index >= gTank.animationArray.length){
     gTank.index = 0;
   }
-  console.log(gTank.index)
+  console.log(gTank.index, bTank.index)
+
+  bTank.draw();
+  bTank.index += 1;
+  if (bTank.index >= bTank.animationArray.length){
+    bTank.index = 0;
+  }
+  
 }
